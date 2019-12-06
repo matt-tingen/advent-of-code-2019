2b8054cbef574c08b039f44de25abae506256acf
Matt Tingen <matt.tingen@gmail.com>
2019-12-06 00:42:39 -0500

    Support immediate mode params

--- src/runIntCode/runIntCode.test.ts
+++ src/runIntCode/runIntCode.test.ts
@@ -1,5 +1,5 @@
 import test, { Macro } from 'ava';
-import runIntCode from './runIntCode';
+import runIntCode, { parseInstructionHead } from './runIntCode';
 
 const testRunIntCode: Macro<[number[], number[]]> = (
   t,
@@ -9,7 +9,7 @@ const testRunIntCode: Macro<[number[], number[]]> = (
   t.deepEqual(runIntCode(startMemory), endMemory);
 };
 testRunIntCode.title = (title, startMemory, endMemory) =>
-  `${startMemory.join(',')} yields ${endMemory.join(',')}`;
+  title || `${startMemory.join(',')} yields ${endMemory.join(',')}`;
 
 test(testRunIntCode, [1, 0, 0, 0, 99], [1 + 1, 0, 0, 0, 99]);
 test(testRunIntCode, [2, 3, 0, 3, 99], [2, 3, 0, 3 * 2, 99]);
@@ -17,10 +17,27 @@ test(testRunIntCode, [2, 4, 4, 5, 99, 0], [2, 4, 4, 5, 99, 99 * 99]);
 test(
   testRunIntCode,
   [1, 1, 1, 4, 99, 5, 6, 0, 99],
-  [30, 1, 1, 4, 2, 5, 6, 0, 99],
+  [5 * 6, 1, 1, 4, 1 + 1, 5, 6, 0, 99],
 );
 test(
   testRunIntCode,
   [1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50],
-  [3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50],
+  [(30 + 40) * 50, 9, 10, 30 + 40, 2, 3, 11, 0, 99, 30, 40, 50],
 );
+
+test('uses param modes', testRunIntCode, [1101, 0, 0, 0, 99], [0, 0, 0, 0, 99]);
+
+const testParseInstructionHead: Macro<[number, number, number[]]> = (
+  t,
+  instructionHead,
+  opCode,
+  paramModes,
+) => {
+  t.deepEqual(parseInstructionHead(instructionHead), { opCode, paramModes });
+};
+testParseInstructionHead.title = (title, instructionHead, opCode, paramModes) =>
+  `${instructionHead} parses to ${opCode}, ${JSON.stringify(paramModes)}`;
+
+test(testParseInstructionHead, 1, 1, []);
+test(testParseInstructionHead, 1111, 11, [1, 1]);
+test(testParseInstructionHead, 1002, 2, [0, 1]);
--- src/runIntCode/runIntCode.ts
+++ src/runIntCode/runIntCode.ts
@@ -1,16 +1,34 @@
-type Memory = number[];
+import _ from 'lodash';
 
-type Operations = Record<number, (...params: number[]) => void>;
+type Memory = number[];
+type Operations = Record<
+  number,
+  (modes: number[], ...params: number[]) => void
+>;
+
+export const parseInstructionHead = (value: number) => {
+  const opCode = value % 100;
+  const paramModes =
+    value >= 100
+      ? Math.floor(value / 100)
+          .toString()
+          .split('')
+          .map(mode => (mode === '1' ? 1 : 0))
+          .reverse()
+      : [];
+
+  return { opCode, paramModes };
+};
 
 class IntCodeComputer {
   constructor(public memory: Memory) {}
 
   private operations: Operations = {
-    1: (a, b, dest) => {
-      this.memory[dest] = this.memory[a] + this.memory[b];
+    1: ([aMode, bMode], a, b, dest) => {
+      this.memory[dest] = this.getParam(a, aMode) + this.getParam(b, bMode);
     },
-    2: (a, b, dest) => {
-      this.memory[dest] = this.memory[a] * this.memory[b];
+    2: ([aMode, bMode], a, b, dest) => {
+      this.memory[dest] = this.getParam(a, aMode) * this.getParam(b, bMode);
     },
   };
 
@@ -24,21 +42,27 @@ class IntCodeComputer {
     } while (increment);
   }
 
+  private getParam(value: number, mode: number) {
+    return mode ? value : this.memory[value];
+  }
+
   private runInstruction(instructionPointer: number): number {
-    const opCode = this.memory[instructionPointer];
+    const { opCode, paramModes } = parseInstructionHead(
+      this.memory[instructionPointer],
+    );
 
     if (opCode === 99) {
       return 0;
     }
 
     const operation = this.operations[opCode];
-    const numParams = operation.length;
+    const numParams = operation.length - 1;
     const params = this.memory.slice(
       instructionPointer + 1,
       instructionPointer + numParams + 1,
     );
 
-    operation(...params);
+    operation(paramModes, ...params);
 
     return numParams + 1;
   }
